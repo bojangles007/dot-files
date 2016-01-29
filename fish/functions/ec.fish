db96e85b17d24ee4625af15c3575a3c2a847ba0a
Stig Brautaset <sbrautaset@laterpay.net>
2016-01-29 11:52:50 +0000

    Fix fish 'ec' function

--- fish/functions/ec.fish
+++ fish/functions/ec.fish
@@ -1,6 +1,6 @@
-function ec --description 'Open files in a new Emacs frame'
+function ec --description 'Open files in new Emacs frames'
         emacsclient -nc $argv[1]
-        for f in $argv[2..-1]:
-                emacsclient -n $f
+        for f in $argv[2..(count $argv)]:
+                emacsclient -nc $f
         end
 end

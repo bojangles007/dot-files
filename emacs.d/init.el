0ec7a11b33cedb37899ee1ad445695b6ca96a382
Stig Brautaset <stig@brautaset.org>
2016-03-15 00:01:18 +0000

    Make changes suggested by flycheck

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,10 +1,14 @@
 ;;; init.el --- Where all the magic begins
-;;
+
+;;; Commentary:
+
 ;; This file loads Org-mode and then loads the rest of our Emacs
-;; initialization from Emacs lisp embedded in literate Org-mode files.
+;; initialization from Emacs Lisp embedded in literate Org-mode files.
 
 ;; I copied this from an example found at
 ;; http://orgmode.org/worg/org-contrib/babel/intro.html#literate-programming
+
+;;; Code:
 
 ;; Initialize packages early, so we load latest org from ELPA later in this
 ;; file.
@@ -13,25 +17,12 @@
 ;; Don't call package-initialize again after we're done with init.el
 (setq package-enable-at-startup nil)
 
-;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org
-;; Mode files.
-(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+;; Load up Org-mode and Org-babel
+(require 'org-install)
+(require 'ob-tangle)
 
-(let* ((org-dir (expand-file-name
-                 "lisp" (expand-file-name
-                         "org" (expand-file-name
-                                "src" dotfiles-dir))))
-       (org-contrib-dir (expand-file-name
-                         "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
-       (load-path (append (list org-dir org-contrib-dir)
-                          (or load-path nil))))
-  ;; load up Org-mode and Org-babel
-  (require 'org-install)
-  (require 'ob-tangle))
-
-;; load up all literate org-mode files in this directory
-(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+;; Load (and tangle) all literate org-mode files in this directory
+(let ((dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name))))
+  (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$")))
 
 ;;; init.el ends here

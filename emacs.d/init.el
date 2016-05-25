13416f76357d0c353f2579e361eaddbf0c74cf13
Stig Brautaset <stig@brautaset.org>
2016-05-25 21:27:27 +0100

    Re-jig init a little
    There's now an init.org file that I manually tangle into init.el. All
    other config is loaded from Capitalised .org files in the same
    directory.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,24 +1,21 @@
-;;; init.el --- Where all the magic begins
-
-;;; Commentary:
-
-;; This file loads Org-mode and then loads the rest of our Emacs
-;; initialization from Emacs Lisp embedded in literate Org-mode files.
-
-;; I copied this from an example found at
-;; http://orgmode.org/worg/org-contrib/babel/intro.html#literate-programming
-
-;;; Code:
-
-;; Initialize packages early, so we load latest org from ELPA later in this
-;; file.
 (package-initialize)
 
-;; Don't call package-initialize again after we're done with init.el
 (setq package-enable-at-startup nil)
 
-;; Load (and tangle) all literate org-mode files in this directory
-(let ((dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name))))
-  (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$")))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;;; init.el ends here
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+(unless (package-installed-p 'use-package)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+
+(let ((dotfiles-dir (file-name-directory (or (buffer-file-name)
+                                             load-file-name))))
+  (mapc #'org-babel-load-file
+        (directory-files dotfiles-dir t "[A-Z]\\w+\\.org$")))

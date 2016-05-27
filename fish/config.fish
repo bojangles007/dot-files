1c3ae6524b982035361e3ebc53ab8d6c2f0a5337
Stig Brautaset <stig@brautaset.org>
2016-05-27 23:10:49 +0100

    Prepend directory of ruby gem binaries rather than push

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,5 @@ export EDITOR
 # Add to path for psql
 set PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
 
-set PATH $PATH $HOME/.gem/ruby/2.0.0/bin
+# Jekyll etc
+set PATH $HOME/.gem/ruby/2.0.0/bin $PATH

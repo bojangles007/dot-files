2f097ec48b93bf9852fc903a2cb09289cc765428
Stig Brautaset <stig@brautaset.org>
2016-05-27 23:11:28 +0100

    Add the path to our emacsclient

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,8 @@ export EDITOR
 # Add to path for psql
 set PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
 
+# emacsclient
+set PATH /Applications/Emacs.app/Contents/MacOS/bin $PATH
+
 # Jekyll etc
 set PATH $HOME/.gem/ruby/2.0.0/bin $PATH

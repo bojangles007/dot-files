26cd921de214931dd0b1b96387c9f47c4d32169c
Stig Brautaset <sbrautaset@laterpay.net>
2016-06-10 16:19:10 +0100

    Try to be more clever about finding PostgreSQL path

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ set EDITOR 'emacsclient -n'
 export EDITOR
 
 # Add to path for psql
-set PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
+set PATH (ls -d1  /Applications/Postgres.app/Contents/Versions/*/bin | tail -1) $PATH
 
 # emacsclient
 set PATH /Applications/Emacs.app/Contents/MacOS/bin $PATH

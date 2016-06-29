5e9cbb1fe9b569d2a91862e9c0a1d51f33da6dae
Stig Brautaset <sbrautaset@laterpay.net>
2016-06-29 15:25:28 +0100

    Try to be more clever in determining Emacs bin directory containing emacsclient

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ export EDITOR
 set PATH (ls -d1  /Applications/Postgres.app/Contents/Versions/*/bin | tail -1) $PATH
 
 # emacsclient
-set PATH /Applications/Emacs.app/Contents/MacOS/bin $PATH
+set PATH (ls -d1 /Applications/Emacs.app/Contents/MacOS/bin-x86_64-* | tail -1) $PATH
 
 # Jekyll etc
 set PATH $HOME/.gem/ruby/2.0.0/bin $PATH

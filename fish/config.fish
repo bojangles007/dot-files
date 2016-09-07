b7910e28105d685e0ab1e36500958f88446587e7
Stig Brautaset <stig@brautaset.org>
2016-09-07 12:48:13 +0100

    Drop a couple items from fish's PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,5 @@
 set EDITOR emacsclient
 
-# Add to path for psql
-set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
-
 # emacsclient
 set PATH /Applications/Emacs.app/Contents/MacOS/bin $PATH
 
-# Jekyll etc
-set PATH $HOME/.gem/ruby/2.3.0/bin $PATH

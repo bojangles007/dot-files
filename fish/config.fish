c6ab09499a47fbe01326209f053a4b02d8a0786a
Stig Brautaset <stig@brautaset.org>
2016-11-03 20:18:35 +0000

    Installing Emacs from source obsoletes this hack

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,9 @@
 set EDITOR emacsclient
 
-# emacsclient
-set PATH /Applications/Emacs.app/Contents/MacOS/bin $PATH
-
 # Ruby gems
 set PATH (echo ~/.gem/ruby/*/bin) $PATH
 
 eval (python -m virtualfish)
-
 
 # autocompletion for AWS CLI; if the aws_completer binary is installed
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
